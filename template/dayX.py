5c4f060e165350a726ed99c151223a3e9a87c697
AmauryDelannoy <amaury.delannoy@ext.leroymerlin.fr>
2022-12-01 13:15:30 +0100

    change the way to read file

--- template/dayX.py
+++ template/dayX.py
@@ -1,6 +1,7 @@
 if __name__ == '__main__':
     with open("input", "r") as input_list:
-        for line in input_list:
+
+        for line in input_list.read().splitlines():
             # Do your job here !
             pass
 
