693cfc4685263b14a0b3876fa5771dbee3fef7f0
AmauryDelannoy <amaury.delannoy@ext.leroymerlin.fr>
2022-12-01 10:45:08 +0100

    add template package for next days

---
+++ template/dayX.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    with open("input", "r") as input_list:
+        for line in input_list:
+            # Do your job here !
+            pass
+
+        print("result here")
