e59a354ee1b1bf111e3fb5759b08d3b8935fa8d5
AmauryDelannoy <amaury.delannoy@ext.leroymerlin.fr>
2022-12-02 10:10:27 +0100

    better day2

--- day2/day2.py
+++ day2/day2.py
@@ -15,18 +15,6 @@ def part1():
     return score
 
 
-def win():
-    return 6
-
-
-def draw():
-    return 3
-
-
-def lose():
-    return 0
-
-
 if __name__ == '__main__':
     with open("input", "r") as input_list:
         # A = Rock, B = Paper, C = Scissors
@@ -35,51 +23,23 @@ if __name__ == '__main__':
         # Part 2
         # X = Lose, Y = Draw, Z = Win
         my_shape = {'X': 1, 'Y': 2, 'Z': 3}
+        my_lose_shape = {'A': 'Z', 'B': 'X', 'C': 'Y'}
+        my_draw_shape = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+        my_win_shape = {'A': 'Y', 'B': 'Z', 'C': 'X'}
+        my_point = {'X': 0, 'Y': 3, 'Z': 6}
         score_part1 = 0
         score_part2 = 0
         for line in input_list.read().splitlines():
             # Do your job here !
             entry = line.split(" ")
-            print(entry)
 
             # Add my selected score shape
             score_part1 += my_shape[entry[1]]
             score_part1 += part1()
 
-            match entry[0]:
-                case 'A':
-                    match entry[1]:
-                        case 'X':
-                            score_part2 += lose()
-                            score_part2 += my_shape['Z']
-                        case 'Y':
-                            score_part2 += draw()
-                            score_part2 += my_shape['X']
-                        case 'Z':
-                            score_part2 += win()
-                            score_part2 += my_shape['Y']
-                case 'B':
-                    match entry[1]:
-                        case 'X':
-                            score_part2 += lose()
-                            score_part2 += my_shape['X']
-                        case 'Y':
-                            score_part2 += draw()
-                            score_part2 += my_shape['Y']
-                        case 'Z':
-                            score_part2 += win()
-                            score_part2 += my_shape['Z']
-                case 'C':
-                    match entry[1]:
-                        case 'X':
-                            score_part2 += lose()
-                            score_part2 += my_shape['Y']
-                        case 'Y':
-                            score_part2 += draw()
-                            score_part2 += my_shape['Z']
-                        case 'Z':
-                            score_part2 += win()
-                            score_part2 += my_shape['X']
+            # Add my win / draw / lose point
+            score_part2 += my_point[entry[1]]
+            score_part2 += my_shape[my_win_shape[entry[0]]]
 
-        print(f"My score part 1 is {score_part1}")
-        print(f"My score part 2 is {score_part2}")
+        print(f"My score part 1 is {score_part1}") # 12772
+        print(f"My score part 2 is {score_part2}") # 11618
