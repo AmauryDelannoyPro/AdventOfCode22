48c563dafc02d49e25d2d3b498f75c9ce91c19c8
Amaury Delannoy <delannoy.amaury.pro@gmail.com>
2022-12-04 09:43:27 +0100

    Add scores in template

--- template/dayX.py
+++ template/dayX.py
@@ -1,8 +1,9 @@
 if __name__ == '__main__':
     with open("input", "r") as input_list:
-
+        score1, score2 = 0, 0
         for line in input_list.read().splitlines():
             # Do your job here !
             pass
 
-        print("result here")
+        print(f"Part 1 : {score1}")  #
+        print(f"Part 2 : {score1}")  #
