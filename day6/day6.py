6fb9d3195c2b44fb24df08d174a2dc0ec4b919b4
AmauryDelannoy <amaury.delannoy@ext.leroymerlin.fr>
2022-12-09 17:11:07 +0100

    day6: correct part2

--- day6/day6.py
+++ day6/day6.py
@@ -1,26 +1,46 @@
-def evaluate_part1():
-    seq = "".join(charlist)
-    for i in range(len(charlist)):
-        if seq.count(charlist[i]) > 1:
+def evaluate_part1(mylist):
+    seq = "".join(mylist)
+    for i in range(len(mylist)):
+        if seq.count(mylist[i]) > 1:
             return False
     return True
 
-charlist = list()
+
+def part1(current_line):
+    charlist1 = list()
+    PART1_LENGHT = 4
+    for idchar, char in enumerate(current_line):
+        if len(charlist1) < PART1_LENGHT:
+            charlist1.append(char)
+            continue
+        if evaluate_part1(charlist1):
+            print(f"Part 1 : {idchar}")  # 1651
+            break
+        else:
+            charlist1.pop(0)
+            charlist1.append(char)
+
+
+
+def part2(current_line):
+    charlist2 = list()
+    PART2_LENGHT = 14
+    for idchar, char in enumerate(current_line):
+        if len(charlist2) < PART2_LENGHT:
+            charlist2.append(char)
+            continue
+        if evaluate_part1(charlist2):
+            print(f"Part 2 : {idchar}")  # 3837
+            break
+        else:
+            charlist2.pop(0)
+            charlist2.append(char)
+
+
 if __name__ == '__main__':
     with open("input", "r") as input_list:
-        score1, score2 = 0, 0
         for line in input_list.read().splitlines():
             # Do your job here !
-            for idchar, char in enumerate(line):
-                if len(charlist) < 4:
-                    charlist.append(char)
-                    continue
-                if evaluate_part1():
-                    score1 = idchar
-                    break
-                else:
-                    charlist.pop(0)
-                    charlist.append(char)
+            part1(line)
+            part2(line)
 
-        print(f"Part 1 : {score1}")  # 1651
-        print(f"Part 2 : {score2}")  #
