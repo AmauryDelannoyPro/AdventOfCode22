99418fdb3f6f82533bac6412991da096657f1bfc
AmauryDelannoy <amaury.delannoy@ext.leroymerlin.fr>
2023-02-23 16:13:49 +0100

    day9: part1 done

--- day9/day9.py
+++ day9/day9.py
@@ -1,52 +1,30 @@
-
-# Classe Map
-#   Width x Height
-#   def addcol
-#       width.insert(0)
-#       or
-#       width.append()
-#   pareil pour addLine
-
-# def moveT
-#   isMoveNeeded ?
-#       update map
-def moveH(direction):
-
-    pass
-
-class World:
-    map = ["s"]
-
-    def add_col(self):
-        self.map.append("")
-
-    def display(self):
-        for line in self.map:
-            for col in line:
-                print(col)
-
-
 if __name__ == '__main__':
     with open("input", "r") as input_list:
         score1, score2 = 0, 0
-        world = World()
-
-        # 1er déplacement, H bouge mais pas T
-        # Après le 1er tour, T est a la position de H au tour précedant
-        # Nan, pas bon si H revient sur ses pas
-        # Avant de bouger T a H-1, on vérifie la distance T<->H.
-        # Si Distance > 1, on fait T = H-1
-
-        # Calcul de map :
-        # option 1 (pas top) : on calcul les diff R/L et U/D
-        # option 2 : faire méthode add colonne (gauche ou droite) et pareil pour ligne
+        h = (0,0)
+        t = (0,0)
+        visited = set()
+        visited.add(t)
 
         for line in input_list.read().splitlines():
             direction, quantite = line.split(" ")
-            # print(direction, quantite)
-            for move in range(1, int(quantite)):
-                # print(direction)
-                moveH(direction)
+            for _ in range(int(quantite)):
+                last_h = h
+                match direction:
+                    case 'U':
+                        h = (h[0], h[1] + 1)
+                    case 'D':
+                        h = (h[0], h[1] - 1)
+                    case 'R':
+                        h = (h[0] + 1, h[1])
+                    case 'L':
+                        h = (h[0] - 1, h[1])
+                diff_x = h[0] - t[0]
+                diff_y = h[1] - t[1]
+                if abs(diff_x) > 1 or abs(diff_y) > 1:
+                    t = (last_h[0], last_h[1])
+                    visited.add(t)
 
-        print(f"Part 1 : {score1}")  #
+        score1 = len(visited)
+        print(f"Part 1 : {score1}")  #5779
         print(f"Part 2 : {score2}")  #
