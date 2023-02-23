17439466fea00151da2c7a7ddd4b76185d6d8d94
AmauryDelannoy <amaury.delannoy@ext.leroymerlin.fr>
2023-02-23 14:48:28 +0100

    day9: chill start

---
+++ day9/day9.py
@@ -0,0 +1,33 @@
+
+# Classe Map
+#   Width x Height
+#   def addcol
+#       width.insert(0)
+#       or
+#       width.append()
+#   pareil pour addLine
+
+# def moveT
+#   isMoveNeeded ?
+#       update map
+
+if __name__ == '__main__':
+    with open("input", "r") as input_list:
+        score1, score2 = 0, 0
+
+        # 1er déplacement, H bouge mais pas T
+        # Après le 1er tour, T est a la position de H au tour précedant
+        # Nan, pas bon si H revient sur ses pas
+        # Avant de bouger T a H-1, on vérifie la distance T<->H.
+        # Si Distance > 1, on fait T = H-1
+
+        # Calcul de map :
+        # option 1 (pas top) : on calcul les diff R/L et U/D
+        # option 2 : faire méthode add colonne (gauche ou droite) et pareil pour ligne
+
+        for line in input_list.read().splitlines():
+            # Do your job here !
+            pass
+
+        print(f"Part 1 : {score1}")  #
+        print(f"Part 2 : {score2}")  #
